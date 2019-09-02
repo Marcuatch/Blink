4870b57ab944f99941fd37ae02d455146afefe2f
MarcosPascual <mpmarcuatch@gmail.com>
2019-09-02 20:11:45 +0200

    Cambio de tiempo

--- Blink/Blink/Blink.ino
+++ Blink/Blink/Blink.ino
@@ -30,7 +30,7 @@ void setup() {
 // the loop function runs over and over again forever
 void loop() {
   digitalWrite(LED_BUILTIN, HIGH);   
-  delay(1000);                       
+  delay(500);                       
   digitalWrite(LED_BUILTIN, LOW);    
-  delay(1000);                       
+  delay(500);                       
 }
